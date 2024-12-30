08e0819ed663282c01bb0ef2b586c7d29d6a9673
Yaman Kumar Malik <yamankumarmalik007@gmail.com>
2024-12-30 14:43:04 +0530

    Adding Recipes

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
-  styleUrl: './recipes.component.css'
+  styleUrl: './recipes.component.css',
 })
-export class RecipesComponent {
-
+export class RecipesComponent implements OnInit {
+  ngOnInit(): void {}
 }
